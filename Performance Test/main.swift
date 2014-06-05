fdbecfe57581ef8cbd78387dea926a8c99931b71
David Green <david_green2@intuit.com>
2014-06-05 10:19:12 -0400

    Initial Commit

---
+++ Performance Test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Performance Test
+//
+//  Created by Green2, David on 6/5/14.
+//  Copyright (c) 2014 Digital Worlds. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
